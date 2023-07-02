14e461ad83d53c6f3017b8f89ee0142c64b582cd
Alejandro Domínguez Ogaz <90666817+Alexdmz1@users.noreply.github.com>
2023-07-02 08:28:38 -0700

    Módulo 2 renombrado y realizado.

---
+++ PythonFastTrackIACXBOSCH/Modulo_2_Crear_y_administrar_proyectos/src/app.py
@@ -0,0 +1,11 @@
+#llamando los módulos
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+now = datetime.now()
+print(now)
+
+now = now + relativedelta(months=1, weeks=1, hour=10)
+
+print(now)
